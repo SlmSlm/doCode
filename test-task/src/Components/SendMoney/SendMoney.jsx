5d7730b51d040f099611151d399273971ce0f5ef
mac <mac@macs-MacBook-Pro.local>
2021-08-31 17:27:14 +0300

    Send money page added

--- test-task/src/Components/SendMoney/SendMoney.jsx
+++ test-task/src/Components/SendMoney/SendMoney.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import card from "../../Images/card.svg";
+import Accounts from "../Accounts/Accounts";
+import sendMoneyRedIcon from "../../Images/RedColorIcons/sendMoneyRedIcon.svg";
+import styles from "./SendMoney.module.css";
 
 const sendMoney = () => {
-  return <div>ololo</div>;
+  return (
+    <div className={styles.sendMessagePage}>
+      <div className={styles.accountsBlock}>
+        <div className={styles.heading}>
+          <img src={card} alt="icon" /> Send Money From:
+          <Accounts />
+        </div>
+      </div>
+      <div className={styles.sendMoneyBlock}>
+        <img src={sendMoneyRedIcon} alt="icon" /> Send Money
+      </div>
+    </div>
+  );
 };
 
 export default sendMoney;
